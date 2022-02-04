d0a1cb841483d0ea4059c642b96e0ba6e7017499
sheldonsheng <shengxuyuan@126.com>
2022-02-04 22:13:43 +0800

    test data update function

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ import investpy
 import pandas as pd
 import datetime
 
+basic_data = investpy.commodities.get_commodity_historical_data(commodity='Gold', from_date='01/01/1900',
+                                                                to_date='01/02/2022')
+basic_data.to_csv('gold_data.csv')
+
+
 def data_update():
     f = open('gold_data.csv', 'r')
     exist_data = pd.read_csv(f, parse_dates=['Date'], index_col=['Date'])
@@ -13,13 +18,12 @@ def data_update():
     today = datetime.date.today()
     to_date = today.strftime("%d/%m/%Y")
     time_mark = today.strftime('%Y-%m-%d')
-    delta_data = investpy.commodities.get_commodity_historical_data(commodity='Gold', from_date=from_date, to_date=to_date)\
-        .iloc[1:, :]
+    delta_data = investpy.commodities.get_commodity_historical_data(commodity='Gold', from_date=from_date,
+                                                                    to_date=to_date).iloc[1:, :]
     updated_data = exist_data.append(delta_data)
     print(updated_data)
     updated_data.to_csv('gold_data_updated_date:' + time_mark + '.csv')
 
 
-#test
-
-
+data_update()
+# test
